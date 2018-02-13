fa3f183c5d73ad6c640cf28778cc8f52fda19d25
Nico Daunt and Sara Hamilton <nico@>
2018-02-12 16:26:01 -0800

    add controller and views for implementation

--- Leetspeak/Controllers/LeetspeakController.cs
+++ Leetspeak/Controllers/LeetspeakController.cs
@@ -1,25 +1,24 @@
 using System.Collections.Generic;
 using System;
 using Microsoft.AspNetCore.Mvc;
-// using Leetspeak.Models;
+using Leetspeak.Models;
 
 namespace Leetspeak.Controllers
 {
   public class LeetspeakController : Controller
   {
-    // [HttpGet("/")]
-    // public ActionResult Index()
-    // {
-    //   return View();
-    // }
-    // [HttpPost("/")]
-    // public ActionResult Display()
-    // {
-    //   // LeetspeakGenerator newLeetspeakGenerator = new LeetspeakGenerator();
-    //   // List<string> model = newLeetspeakGenerator.Leetspeak(Request.Form["input-string"]);
-    //   //
-    //   return View();
-    // }
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+    [HttpPost("/")]
+    public ActionResult Display()
+    {
+      LeetspeakGenerator newLeetspeakGenerator = new LeetspeakGenerator();
+      string model = newLeetspeakGenerator.Leetspeak(Request.Form["input-string"]);
+      return View("Display", model);
+    }
 
   }
 }
--- Leetspeak/Models/Leetspeak.cs
+++ Leetspeak/Models/Leetspeak.cs
@@ -1,13 +1,20 @@
 using System.Collections.Generic;
 using System;
 
-namespace Leetspeak
+namespace Leetspeak.Models
 {
   public class LeetspeakGenerator
   {
     public string Leetspeak(string inputString)
     {
-      return "";
+      string translatedString = inputString.Replace('e','3').Replace('E','3').Replace('o','0').Replace('O','0').Replace('I','1').Replace('s','z').Replace('S','Z');
+      if(inputString[0] == 's' || inputString[0] == 'S')
+      {
+        char[] chars = translatedString.ToCharArray();
+        chars[0] = inputString[0];
+        translatedString = new string(chars);
+      }
+      return translatedString;
     }
   }
 }
